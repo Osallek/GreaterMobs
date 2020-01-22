bd8543bb54e66a6bf2a564f7f5f82c8045762418
Nyvlem <Nyvlem59@hotmail.fr>
2020-01-22 10:57:33 +0100

    Init

---
+++ src/main/java/com/osallek/GreaterMobs.java
@@ -0,0 +1,13 @@
+package com.osallek;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GreaterMobs extends JavaPlugin {
+
+    public static GreaterMobs GREATER_MOBS;
+
+    @Override
+    public void onEnable() {
+        GREATER_MOBS = this;
+    }
+}
